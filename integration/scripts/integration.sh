73b11f85dd09ff1e7bac4ab4ce2d3adc7b1f2ffe
Kyle Mestery <mestery@mestery.com>
2022-07-07 11:19:18 -0500

    issue65: Move bash into actual script
    This moves the bash scripting from the GitHub Action file into it's own
    actual bash script, making it easier to maintain.
    
    Closes #65
    
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

---
+++ integration/scripts/integration.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2022 Intel Corporation
+
+INT_BUILD_START=build-start
+INT_RUN_TESTS=run-tests
+INT_LOGS=logs
+INT_STOP=stop
+
+usage() {
+    echo ""
+    echo "Usage: integration.sh [${INT_BUILD_START} | ${INT_RUN_TESTS} | ${INT_LOGS} | ${INT_STOP}]"
+    echo ""
+}
+
+build_and_start_containers() {
+    docker-compose -f docker-compose.yml -f docker-compose.dpu.yml -f docker-compose.otel.yml -f docker-compose.pxe.yml down
+    docker network prune --force
+    docker-compose -f docker-compose.yml -f docker-compose.dpu.yml -f docker-compose.otel.yml -f docker-compose.pxe.yml up -d
+}
+
+run_integration_tests() {
+    docker-compose -f docker-compose.yml -f docker-compose.dpu.yml -f docker-compose.otel.yml -f docker-compose.pxe.yml ps
+    echo wait 5s... && sleep 5s
+    curl --fail http://127.0.0.1:8001/redfish/v1/Systems/437XR1138R2
+    curl --fail http://127.0.0.1:8002/redfish/v1/Systems/437XR1138R2
+    curl --fail http://127.0.0.1:8082/var/lib/tftpboot/
+    curl --fail http://127.0.0.1:9090/
+    docker-compose -f docker-compose.pxe.yml exec -T pxe dnf install -y nmap tftp
+    docker-compose -f docker-compose.pxe.yml exec -T pxe nmap --script broadcast-dhcp-discover
+    docker-compose -f docker-compose.pxe.yml exec -T pxe nmap --script broadcast-dhcp-discover | grep "Server Identifier: 10.127.127.3"
+    docker-compose -f docker-compose.pxe.yml exec -T pxe curl --fail http://10.127.127.3:8082/var/lib/tftpboot/
+    docker-compose -f docker-compose.pxe.yml exec -T pxe tftp 10.127.127.3 -v -c get grubx64.efi
+}
+
+acquire_logs() {
+    docker-compose logs || true
+    netstat -an || true
+    ifconfig -a || true
+    docker inspect "$(docker-compose -f docker-compose.yml -f docker-compose.dpu.yml -f docker-compose.otel.yml -f docker-compose.pxe.yml ps -aq)" || true
+}
+
+stop_containers() {
+    docker-compose -f docker-compose.yml -f docker-compose.dpu.yml -f docker-compose.otel.yml -f docker-compose.pxe.yml down
+}
+
+if [ "$#" -lt 1 ]
+then
+    usage
+    exit 1
+fi
+
+if [ "$1" == "${INT_BUILD_START}" ]
+then
+    build_and_start_containers
+elif [ "$1" == "${INT_RUN_TESTS}" ]
+then
+    run_integration_tests
+elif [ "$1" == "${INT_LOGS}" ]
+then
+    acquire_logs
+elif [ "$1" == "${INT_STOP}" ]
+then
+    stop_containers
+else
+    echo "Invalid argument: $1"
+    usage
+    exit 1
+fi
