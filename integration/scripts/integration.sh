13a7213f7fee1479b278ab06cd1e9d6917ad2fa6
Boris Glimcher <Boris.Glimcher@emc.com>
2022-07-11 20:29:01 +0300

    introduce .env
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- integration/scripts/integration.sh
+++ integration/scripts/integration.sh
@@ -25,17 +25,17 @@ usage() {
 }
 
 build_containers() {
-    bash -c "${DC} -f docker-compose.yml -f docker-compose.xpu.yml -f docker-compose.otel.yml -f docker-compose.spdk.yml -f docker-compose.pxe.yml build --parallel"
+    bash -c "${DC} build --parallel"
 }
 
 start_containers() {
-    bash -c "${DC} -f docker-compose.yml -f docker-compose.xpu.yml -f docker-compose.otel.yml -f docker-compose.spdk.yml -f docker-compose.pxe.yml down"
+    bash -c "${DC} down"
     docker network prune --force
-    bash -c "${DC} -f docker-compose.yml -f docker-compose.xpu.yml -f docker-compose.otel.yml -f docker-compose.spdk.yml -f docker-compose.pxe.yml up -d"
+    bash -c "${DC} up -d"
 }
 
 run_integration_tests() {
-    bash -c "${DC} -f docker-compose.yml -f docker-compose.xpu.yml -f docker-compose.otel.yml -f docker-compose.spdk.yml -f docker-compose.pxe.yml ps"
+    bash -c "${DC} ps"
     echo wait 5s... && sleep 5s
     curl --fail http://127.0.0.1:8001/redfish/v1/Systems/437XR1138R2
     curl --fail http://127.0.0.1:8002/redfish/v1/Systems/437XR1138R2
@@ -49,25 +49,25 @@ run_integration_tests() {
     sshpass -p 123456 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 2208  bmc@127.0.0.1 hostname
     sshpass -p 123456 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 2207  xpu@127.0.0.1 hostname
     sshpass -p 123456 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 2209  bmc@127.0.0.1 hostname
-    bash -c "${DC} -f docker-compose.pxe.yml exec -T pxe nmap --script broadcast-dhcp-discover"
-    bash -c "${DC} -f docker-compose.pxe.yml exec -T pxe nmap --script broadcast-dhcp-discover" | grep "Server Identifier: 10.127.127.3"
-    bash -c "${DC} -f docker-compose.pxe.yml exec -T pxe curl --fail http://10.127.127.3:8082/var/lib/tftpboot/"
-    bash -c "${DC} -f docker-compose.pxe.yml exec -T pxe tftp 10.127.127.3 -v -c get grubx64.efi"
-    bash -c "${DC} -f docker-compose.spdk.yml exec -T spdk-target /root/spdk/build/examples/identify -r 'traddr:10.127.127.4 trtype:TCP adrfam:IPv4 trsvcid:4420'"
-    bash -c "${DC} -f docker-compose.xpu.yml  exec -T xpu-spdk /root/spdk/build/examples/identify    -r 'traddr:10.127.127.4 trtype:TCP adrfam:IPv4 trsvcid:4420'"
-    bash -c "${DC} -f docker-compose.spdk.yml exec -T spdk-target /root/spdk/build/examples/perf     -r 'traddr:10.127.127.4 trtype:TCP adrfam:IPv4 trsvcid:4420' -c 0x1 -q 1 -o 4096 -w randread -t 10"
-    bash -c "${DC} -f docker-compose.xpu.yml  exec -T xpu-spdk /root/spdk/build/examples/perf        -r 'traddr:10.127.127.4 trtype:TCP adrfam:IPv4 trsvcid:4420' -c 0x1 -q 1 -o 4096 -w randread -t 10"
+    bash -c "${DC} exec -T pxe nmap --script broadcast-dhcp-discover"
+    bash -c "${DC} exec -T pxe nmap --script broadcast-dhcp-discover" | grep "Server Identifier: 10.127.127.3"
+    bash -c "${DC} exec -T pxe curl --fail http://10.127.127.3:8082/var/lib/tftpboot/"
+    bash -c "${DC} exec -T pxe tftp 10.127.127.3 -v -c get grubx64.efi"
+    bash -c "${DC} exec -T spdk-target /root/spdk/build/examples/identify -r 'traddr:10.127.127.4 trtype:TCP adrfam:IPv4 trsvcid:4420'"
+    bash -c "${DC} exec -T xpu-spdk /root/spdk/build/examples/identify    -r 'traddr:10.127.127.4 trtype:TCP adrfam:IPv4 trsvcid:4420'"
+    bash -c "${DC} exec -T spdk-target /root/spdk/build/examples/perf     -r 'traddr:10.127.127.4 trtype:TCP adrfam:IPv4 trsvcid:4420' -c 0x1 -q 1 -o 4096 -w randread -t 10"
+    bash -c "${DC} exec -T xpu-spdk /root/spdk/build/examples/perf        -r 'traddr:10.127.127.4 trtype:TCP adrfam:IPv4 trsvcid:4420' -c 0x1 -q 1 -o 4096 -w randread -t 10"
 }
 
 acquire_logs() {
-    bash -c "${DC} -f docker-compose.yml -f docker-compose.xpu.yml -f docker-compose.otel.yml -f docker-compose.spdk.yml -f docker-compose.pxe.yml ps -a"
-    bash -c "${DC} -f docker-compose.yml -f docker-compose.xpu.yml -f docker-compose.otel.yml -f docker-compose.spdk.yml -f docker-compose.pxe.yml logs" || true
+    bash -c "${DC} ps -a"
+    bash -c "${DC} logs" || true
     netstat -an || true
     ifconfig -a || true
 }
 
 stop_containers() {
-    bash -c "${DC} -f docker-compose.yml -f docker-compose.xpu.yml -f docker-compose.otel.yml -f docker-compose.spdk.yml -f docker-compose.pxe.yml down"
+    bash -c "${DC} down"
 }
 
 if [ "$#" -lt 1 ]
