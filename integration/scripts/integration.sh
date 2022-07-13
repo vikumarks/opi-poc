c826f13db8ca3f9d70df36b435c3cc441345e13c
Boris Glimcher <Boris.Glimcher@emc.com>
2022-07-13 22:17:38 +0300

    integration: split HTTP to a separate container
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- integration/scripts/integration.sh
+++ integration/scripts/integration.sh
@@ -72,7 +72,7 @@ run_integration_tests() {
     bash -c "${DC} exec -T pxe nmap --script broadcast-dhcp-discover"
     bash -c "${DC} exec -T pxe cat /var/lib/dhcpd/dhcpd.leases"
     bash -c "${DC} exec -T pxe nmap --script broadcast-dhcp-discover" | grep "Server Identifier: 10.127.127.3"
-    bash -c "${DC} exec -T pxe curl --fail http://10.127.127.3:8082/var/lib/tftpboot/"
+    bash -c "${DC} exec -T pxe curl --fail http://10.127.127.16:8082/var/lib/tftpboot/"
     bash -c "${DC} exec -T pxe tftp 10.127.127.3 -v -c get grubx64.efi"
     bash -c "${DC} exec -T sztp ./run-sztpd-test.sh"
     bash -c "${DC} exec -T spdk-target /root/spdk/build/examples/identify -r 'traddr:10.127.127.4 trtype:TCP adrfam:IPv4 trsvcid:4420'"
